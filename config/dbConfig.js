28f3f249105abb95ef81b84f0842dfaeb722c609
Heena <heenamulani509@gmail.com>
2026-03-01 11:52:49 +0530

    fix: Update database connection configuration to use environment variables

--- config/dbConfig.js
+++ config/dbConfig.js
@@ -1,10 +1,18 @@
 const mongoose = require('mongoose');
 
+// Replace with your actual connection details
+const username = process.env.USER_ADMIN;
+const password = process.env.USER_PASS;
+const cluster = process.env.DB_CLUSTER;
+const dbName = process.env.DB_NAME; // Your desired DB name
 
-const connectDB = async ()=>{
-  await mongoose.connect(process.env.DATABASE_URL)
-    .then(() => console.log('Connected to MongoDB'))
-    .catch(err => console.error('Could not connect to MongoDB', err));
+const uri = `mongodb+srv://${username}:${password}@${cluster}/${dbName}?retryWrites=true&w=majority`;
+
+const connectDB = async () => {
+  mongoose.connect(uri)
+.then(() => console.log(`✅ Connected to MongoDB Atlas database: ${dbName}`))
+.catch(err => console.error('❌ MongoDB connection error:', err));
 }
+
 
 module.exports = connectDB;
